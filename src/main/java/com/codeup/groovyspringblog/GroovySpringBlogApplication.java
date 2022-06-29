f88e08e080c48e1731911aa69512331efbebc7fb
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-06-29 14:49:16 -0400

    first-commit

---
+++ src/main/java/com/codeup/groovyspringblog/GroovySpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.groovyspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovySpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroovySpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/groovyspringblog/GroovySpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.groovyspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovySpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
